f5415da0c5b56cef9689da331e1de87900c3b022
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-03-02 12:26:06 +0530

    intial commit

---
+++ first-project/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using first_project.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace first_project.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Each function inside controller, is called - "action"
+
+        // What is IActionResult ?
+        // It is an interface that includes, different response datatypes that an “action” can return. For ex - JSON, View, string, integer, so on…
+
+        // default route for each controller - Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // /student
+        public IActionResult GetStudent()
+        {
+            // { Name= "Tanveer", Age=25 } -> giving this creates new record in table
+            var student = new Student() { Name= "Tanveer", Age=25 };
+            return View(student);
+        }
+
+
+
+        // /student
+        public IActionResult GetStudentData(int id, int queryParam)
+        {
+            return Content("Path Param = " + id + " & " + "Query Param = " + queryParam);
+        }
+
+    }
+}
+
---
+++ first-project/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace first_project.Models
+{
+    // table model
+	public class Student
+	{
+        public int StudentId { get; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
+
