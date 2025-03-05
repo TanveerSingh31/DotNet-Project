a4e80b99504ca4830f9332e21d998e5293fa0b0a
TanveerSingh31 <tanveersinghnayyar@gmail.com>
2025-03-05 11:43:42 +0530

    reading data

---
+++ first-project/Controllers/StudentControllerDb.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using first_project.Data;
+
+namespace first_project.Controllers
+{
+    // This controller talks with the Db.
+    public class StudentControllerDb : Controller
+    {
+
+        private MyAppContext _context;
+
+        // Contructor 
+        public StudentControllerDb(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Read Data
+
+            // To access models, we need to use the dbContext 
+            // This is lambda function in C# --> s => s.Id > 1
+            // And Where, ToListSync, These methods are part of "LINQ"
+            var studentsData = await _context.Students.Where(s => s.Name == "Tanveer Singh").ToListAsync();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(studentsData);
+
+            return Ok(studentsData);
+        }
+    }
+}
